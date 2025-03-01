46f5532b02f892f0b2e44b68ad79ede403e596d5
Dmitriy <95078422+Amid3222@users.noreply.github.com>
2025-03-01 13:03:46 +0300

    :

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+//
         CounterClass even = new CounterClass("Счетчик Четных"); // создаем объедки
         CounterClass noEven = new CounterClass("Счетчик Нечетных");
 
