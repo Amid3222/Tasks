ec6f2b43decbd3b427d7aa4a6f7d3f5728ee2b8d
Dmitriy <95078422+Amid3222@users.noreply.github.com>
2025-03-03 16:27:30 +0300

    Коммит2

--- src/com/walking/counterAggregation/CounterService.java
+++ src/com/walking/counterAggregation/CounterService.java
@@ -1,4 +1,44 @@
 package com.walking.counterAggregation;
 
+import java.util.Arrays;
+
 public class CounterService {
+
+    //поля
+    private Counter[] counterArr = new Counter[10];
+
+
+    //методы(добавить к массиву)
+    public void addtoArr(Counter counter) {
+        int b = 0;
+        counterArr[b] = counter;
+    }
+    //показать массив счетчиков
+    public void showCounters() {
+        System.out.println(Arrays.toString(counterArr));
+    }
+    //получить доступ к счетчику по имени
+    public Counter getAccesByName(String thename) {
+
+        for (Counter c : counterArr) {
+            if (c.getName().equals(thename)) return c;
+        }
+        return null;
+
+    }
+    //изменить значение счетчика
+    public void setterViper(Counter c, int x) {
+        c.setValue(x);
+    }
+    //++1 или ++ число
+    public void incremerOn(Counter c, int y) {
+        int x = c.getValue();
+        if (y == 0) {
+            c.setValue(++x);
+        } else {
+            c.setValue(x + y);
+        }
+    }
+
+
 }
--- src/com/walking/counterAggregation/Main.java
+++ src/com/walking/counterAggregation/Main.java
@@ -1,1 +1,13 @@
-package com.walking.
+package com.walking.counterAggregation;
+
+public class Main {
+    public static void main(String[] args) {
+        CounterService service = new CounterService();
+        Counter Gaz = new Counter("Газ", "м. куб");
+
+        service.addtoArr(Gaz);
+        service.showCounters();
+
+
+    }
+}
